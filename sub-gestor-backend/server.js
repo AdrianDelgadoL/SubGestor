f0c03e47d8537a62321ce7ba887817a9133c15de
Marc Sala <salamarc6@gmail.com>
2021-04-16 13:24:44 +0200

    Connection from Back-End to MongoDB established, needs enviorement var

---
+++ sub-gestor-backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const PORT = 4000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb+srv://dbAdmin:RJQzSdo5xNa4PR@subgestorcluster.uo0n2.mongodb.net/SubGestor?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
+
+app.listen(PORT, function() {
+    console.log("Server is running on Port: " + PORT);
+});
