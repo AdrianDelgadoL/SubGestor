4ada8439988f6925e3b9312a5f7d9e142a2bdeb0
Marc Sala <salamarc6@gmail.com>
2021-04-16 16:47:12 +0200

    Front-End and Back-End connected using axios

--- sub-gestor-code/src/App.js
+++ sub-gestor-code/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {hello: ""};
+  }
+
+  componentDidMount() {
+      axios.get('http://localhost:4000/hello/')
+          .then(response => {
+              this.setState({ hello: response.data[0].name });
+          })
+          .catch(function (error){
+              console.log(error);
+          })
+  }
+  
+  render() {
+    return (
+      
+        <div className="container">
+          <h2>MERN-Stack Hello App</h2>
+          <p> { this.state.hello }</p>
+        </div>
+      
+    );
+  }
 }
 
 export default App;
