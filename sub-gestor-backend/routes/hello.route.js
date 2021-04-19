8f710e8990f3fe8043b444742eef1e09ab3975b1
Marc Sala <salamarc6@gmail.com>
2021-04-19 11:44:53 +0200

    Little back-end documentation

--- sub-gestor-backend/routes/hello.route.js
+++ sub-gestor-backend/routes/hello.route.js
@@ -2,6 +2,10 @@ const express = require ('express');
 const router = express.Router();
 const Hello = require('../models/helloworld.model');
 
+// Para cada end-point se tiene que definir una peticion. En este caso le decimos al router que las peticiones
+// get a / (recordad que en el server.js este router esta asociado a /hello  y por lo tanto esta hace referencia
+// a la ruta GET /hello/) y dentro de esta le especificamos que utilice el modelo de Hello y mediante la función
+// find, recoga todos los documentos y los guarde en el res (response, este lo utiliza el front-end para ver la información de back-end)
 router.get('/', (req, res) => {
     Hello.find(function(err, hello) {
         if (err) {
@@ -12,4 +16,5 @@ router.get('/', (req, res) => {
     });
 });
 
+// Para que otros bloques lo puedan usar, se debe exportar el router
 module.exports = router;
--- sub-gestor-backend/server.js
+++ sub-gestor-backend/server.js
@@ -9,6 +9,8 @@ const helloRoutes = require('./routes/hello.route');
 app.use(cors());
 app.use(bodyParser.json());
 
+// TODO: Pasarlo a fichero de configuracion para no tenerlo aqui puesto
+// Nos conectamos al servidor de mongoDB.
 mongoose.connect('mongodb+srv://dbAdmin:RJQzSdo5xNa4PR@subgestorcluster.uo0n2.mongodb.net/SubGestor?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 
@@ -19,6 +21,8 @@ connection.once('open', function() {
 /***********
 ** Routes **
 ************/
+// Para cada fichero de routes, tenemos que importarlo (tal y como se hace arriba) y asociarlo a una ruta. En este caso utilizamos
+// el router de hello asociandolo a la ruta /hello
 app.use('/hello', helloRoutes);
 
 app.listen(PORT, function() {
