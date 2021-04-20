092aed421c0ccef9772afd675387c6ca9cfedaa0
1494116 <daniel.calvor@e-campus.uab.cat>
2021-04-20 22:20:13 +0200

    estructura menu desplegable preguntas FAQ + css hecho

--- sub-gestor-code/src/components/FAQ.component.js
+++ sub-gestor-code/src/components/FAQ.component.js
@@ -1,26 +1,43 @@
-
-import React, { Component, useState } from 'react';
-
+import React, { Component } from 'react'
 import './FAQ.component.css'
-import Accordian from './acordion.js'
-
+import Accordion from './acordion.js'
 export default class FAQ extends Component{
-   
-    render(){
-        const [active,setActive]=useState("titll1");
-        
-        
-        return(
-            <div className=" prueba">
-                <Accordian title="HOLA1" active={active} setActive={setActive}/>
-                <Accordian title="HOLA1" active={active} setActive={setActive}/>
-                <Accordian title="HOLA1" active={active} setActive={setActive}/>
-                <Accordian title="HOLA1" active={active} setActive={setActive}/>
 
+    state={
+        data:[
+            {
+                title:"pregunta1",
+                texts:"descargate abc de efeffe"
+            },
+            {
+                title:"pregunta2",
+                texts:"estudia"
+            },
+            {
+                title:"pregunta3",
+                texts:"bla bla bla"
+            }
+
+        ]
+    }
+
+
+
+    render(){
+
+        const {data}=this.state;
+        return(
+            <div className="accordion">
+
+                {data.map((item,index)=>{
+                    return <Accordion key={index} title={item.title} texts={item.texts}/>
+                 })}
+               
             </div>
 
+            );
 
-        );
     }
+
 
 }
