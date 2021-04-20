f5a7039f77a36f46402aa5e0de51cc327c0eaedf
1494116 <daniel.calvor@e-campus.uab.cat>
2021-04-20 19:40:49 +0200

    version con css mal

--- sub-gestor-code/src/components/FAQ.component.js
+++ sub-gestor-code/src/components/FAQ.component.js
@@ -1,48 +1,26 @@
-import axios from 'axios';
-import React, { Component } from 'react';
+
+import React, { Component, useState } from 'react';
 
 import './FAQ.component.css'
-
+import Accordian from './acordion.js'
 
 export default class FAQ extends Component{
-
-        constructor(props){
-            super(props);
-            this.state={faqs:[]};
-        }
-
-        componentDidMount(){
-            axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(res=> {
-                const faqs=res.data.slice(0,10);
-                this.setState({faqs});
-            })
-        }
+   
     render(){
+        const [active,setActive]=useState("titll1");
+        
+        
         return(
-            <div>
-                <div className="container">
-                    <section className="section">
-                        <h1 className="title">FAQ</h1>
-                        <h2 className="subtitle">abcde</h2>
+            <div className=" prueba">
+                <Accordian title="HOLA1" active={active} setActive={setActive}/>
+                <Accordian title="HOLA1" active={active} setActive={setActive}/>
+                <Accordian title="HOLA1" active={active} setActive={setActive}/>
+                <Accordian title="HOLA1" active={active} setActive={setActive}/>
 
-                        <div className="columns">
-                            {this.state.faqs.map(faq =>
-                                <div className="column-is-one-third" v-for="faq of faqs">
-                                    <div className="card">
-                                    <div className="card-content">
-                                        <p className="title1">{faq.title}</p>
-                                        <p className="answer">{faq.body}</p>
-                                    </div>
-                                    </div>
-                                                
-                                </div>
-                                )}
-                        </div>
-                         </section>
-                </div>
-                </div>
+            </div>
+
 
         );
     }
+
 }
