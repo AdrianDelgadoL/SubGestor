c0c51bc574ca465c3ec956e4dddf7fb8ece10127
german <gerkmaik777@gmail.com>
2021-04-22 10:58:14 +0200

    Separacio en divs de les dades del detall

---
+++ sub-gestor-code/src/components/subDetail.component.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
+
+
+
+export default class subDetail extends Component {
+    render() {
+        return (
+            <div className="grid-container">
+                <div className="grid-container-header">
+                    <div className="image">
+
+                    </div>
+                    <div className="name">
+
+                    </div>
+                    <div className="modifyButton">
+
+                    </div>
+                    <div className="deleteButton">
+
+                    </div>
+                </div>
+                <div className="grid-container-price">
+                    <div className="datePayment">
+                    
+                    </div>
+                    <div className="frequency">
+                    
+                    </div>
+                    <div className="price">
+                    
+                    </div>
+                    <div className="currency">
+                    
+                    </div>
+                </div>
+                <div className="grid-container-extra">
+                    <div className="freeTrial">
+                    
+                    </div>
+                    <div className="dateEndTrial">
+
+                    </div>
+                    <div className="hasEnd">
+                    
+                    </div>
+                    <div className="dateEnd">
+                    
+                    </div>
+                </div>
+                <div className="grid-container-information">
+                    <div className="url">
+                    
+                    </div>
+                    <div className="startDate">
+                    
+                    </div>
+                    <div className="tags">
+                    
+                    </div>
+                    <div className="description">
+                    
+                    </div>
+                </div>
+            </div>
+            
+        )
+    }
+}
