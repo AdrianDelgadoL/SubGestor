21d9701d2f87b538429f3a4436b2cca387c34734
german <gerkmaik777@gmail.com>
2021-04-27 13:13:28 +0200

    connexio backend subscripcio v2, falta testejar

--- sub-gestor-code/src/pages/subDetails/subDetail.component.js
+++ sub-gestor-code/src/pages/subDetails/subDetail.component.js
@@ -12,17 +12,48 @@ const SubDetail = (props) => {
     const userDetails = useAuthState();
     //props.match.params.id -> id de consulta
     // /detail/:id
+    const [name, setName] = useState(null);
+    const [datePayment, setDatePayment] = useState(null);
+    const [frequency, setFrequency] = useState(null);
+    const [price, setPrice] = useState(null);
+    const [currency, setCurrency] = useState(null);
+    const [freeTrial, setFreeTrial] = useState(null);
+    const [dateEndTrial, setDateEndTrial] = useState(null);
+    const [hasEnd, setHasEnd] = useState(null);
+    const [dateEnd, setDateEnd] = useState(null);
+    const [url, setUrl] = useState(null);
+    const [startDate, setStartDate] = useState(null);
+    const [tags, setTags] = useState(null);
+    const [description, setDescription] = useState(null);
+    const [imgSrc, setImgSrc] = useState(null);
+
     const getSub = () => {
         console.log(props.match.params.id);
         axios.get('http://localhost:4000/'+ props.match.params.id, {headers: {"x-auth-token": userDetails.token}})
         .then(response => {
             console.log(response.data);
+            setName(name = response.data.name);
+            setImgSrc(imgSrc = response.data.img_src);
+            setDatePayment(datePayment = response.data.charge_date);
+            setFrequency(frequency = response.data.frequency);
+            setPrice(price = response.data.price);
+            setCurrency(currency = response.data.currency);
+            setFreeTrial(freeTrial = response.data.free_trial);
+            setDateEndTrial(dateEndTrial = response.data.free_trial_end);
+            setHasEnd(hasEnd = response.data); //falta en el subscription.model.js
+            setDateEnd(dateEnd = response.data.end_date);
+            setUrl(url = response.data.url);
+            setStartDate(startDate = response.data.start_date);
+            setTags(tags = response.data.tags);
+            setDescription(description = response.data.description);
         })
         .catch(function (error){ //El response devuelve algo distinto a 2xx, por lo tanto hay error
             console.log(error);
+            if(error.response) {
+                break; //tractar si hi ha resposta d'error
+            }
         })
     };
-
 
     return (
         <div>
@@ -33,7 +64,7 @@ const SubDetail = (props) => {
                         <img id="logo" alt="imagen aleatoria" src="https://picsum.photos/700/400?random"></img>
                     </div>
                     <div className="name">
-                        <input type="text" id="name" defaultValue="Nombre suscripción"></input>
+                        <input type="text" id="name" defaultValue={name}></input>
                     </div>
                     <div className="modifyButton">
                         <input type="button" id="modifyButton" value="Guardar cambios"></input>
@@ -45,22 +76,22 @@ const SubDetail = (props) => {
                 <div className="grid-container-price">
                     <div className="datePayment">
                         <label for="datePayment">Fecha de pago:</label><br />
-                        <input type="date" id="datePayment" defaultValue="2021-04-22"></input>
+                        <input type="date" id="datePayment" defaultValue={datePayment}></input>
                     </div>
                     <div className="frequency">
                         <label for="frequency">Frecuencia:</label><br />
-                        <select id="frequency">
+                        <select id="frequency" defaultValue={frequency}>
                             <option value="mensual">Mensual</option>
                             <option value="anual">Anual</option>
                         </select>
                     </div>
                     <div className="price">
                         <label for="price">Precio:</label><br/>
-                        <input type="number" id="price" defaultValue="7.5"></input>
+                        <input type="number" id="price" defaultValue={price}></input>
                     </div>
                     <div className="currency">
                         <label for="currency">Divisa:</label><br />
-                        <select id="currency">
+                        <select id="currency" defaultValue={currency}>
                             <option value="EUR">EUR</option>
                             <option value="USD">USD</option>
                             <option value="GBP">GBP</option>
@@ -69,37 +100,37 @@ const SubDetail = (props) => {
                 </div>
                 <div className="grid-container-extra">
                     <div className="freeTrial">
-                        <label for="freeTrial"><input type="checkbox" id="freeTrial"></input>Periodo de prueba</label><br />
+                        <label for="freeTrial"><input type="checkbox" id="freeTrial" defaultValue={freeTrial}></input>Periodo de prueba</label><br />
                     </div>
                     <div className="dateEndTrial">
                         <label for="dateEndTrial">Fecha de vencimiento:</label><br />
-                        <input type="date" id="dateEndTrial" defaultValue="2021-04-22"></input>
+                        <input type="date" id="dateEndTrial" defaultValue={dateEndTrial}></input>
                     </div>
                     <div className="hasEnd">
-                    <label for="hasEnd"><input type="checkbox" id="hasEnd"></input>Fecha de finalización</label><br />
+                    <label for="hasEnd"><input type="checkbox" id="hasEnd" defaultValue={hasEnd}></input>Fecha de finalización</label><br />
                     </div>
                     <div className="dateEnd">
                         <label for="dateEnd">Fecha de finalización:</label><br />
-                        <input type="date" id="dateEnd" defaultValue="2021-04-22"></input>
+                        <input type="date" id="dateEnd" defaultValue={dateEnd}></input>
                     </div>
                 </div>
                 <div className="grid-container-information">
                     <div className="url">
                         <label for="url">URL para desuscribirse:</label><br />
-                        <input type="url" id="url" defaultValue="https://www.google.com/"></input>
+                        <input type="url" id="url" defaultValue={url}></input>
                     </div>
                     <div className="startDate">
                         <label for="startDate">Fecha de inicio:</label><br />
-                        <input type="date" id="startDate" defaultValue="2021-04-22"></input>
+                        <input type="date" id="startDate" defaultValue={startDate}></input>
                     </div>
                     <div className="tags">
                         <label for="tags">Tags (separados por una coma):</label><br />
-                        <input type="text" id="tags" defaultValue="films,shared"></input>
+                        <input type="text" id="tags" defaultValue={tags}></input>
                     </div>
                     <div className="description">
                         <label for="description">Descripción:</label><br />
                         <textarea rows="4" cols="50" id="description" form="">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tristique massa sed nisi efficitur, eget aliquet sapien facilisis. Donec faucibus ex elit, a egestas nulla accumsan ut. Nunc vitae nisl elementum, convallis est quis, tempus nulla. Morbi pharetra aliquam ipsum, sit amet dictum mauris ultrices pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque est lorem, mollis non tortor non, convallis dictum ligula. Donec et luctus felis. Duis commodo dolor eu dolor dignissim, et fringilla orci commodo. Sed elementum at nisl sit amet imperdiet. Ut a tincidunt nulla. Proin commodo tempor libero nec sagittis. Sed tincidunt magna ut mattis dapibus. In at rutrum metus. Ut vitae dolor vitae turpis egestas efficitur sed vel leo. 
+                        {description}
                         </textarea>
                     </div>
                 </div>
