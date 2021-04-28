63b453d362f800356823df95e38fe1d6bd846ba8
1494116 <daniel.calvor@e-campus.uab.cat>
2021-04-28 17:03:41 +0200

    clases en css Homepage + textos homepage

--- sub-gestor-code/src/pages/homePage/homePage.component.js
+++ sub-gestor-code/src/pages/homePage/homePage.component.js
@@ -8,45 +8,46 @@ export default class HomePage extends Component {
     render() {
         return (
             <div className="homePage">
-                <h2 className="titulo">SubGestor</h2>
-                <div className="text">
-                    <h3 className="eslogan">Gestiona tus suscripciones de forma sencilla y mantén el control de tus servicios contratados</h3>
+                <h2 className="homePage_titulo">SubGestor</h2>
+                <div className="homePage_text">
+                    <h3 className="homePage_eslogan">Gestiona tus suscripciones de forma sencilla y mantén el control de tus servicios contratados</h3>
                     <form method="get" action="/signIn">
-                        <button className="boton"type="submit"><span></span>Empieza ya</button>
+                        <button className="homePage_boton"type="submit"><span className="homePage_span"></span>Empieza ya</button>
                     </form>
                 </div>  
                 
-                <body className="cuerpo">  
-                <div class="imagenes">
-                <div class="imagen-1">
-                <div class="c-img">
-                    <img class="foto1"src={foto1} alt=""></img>
-                    <div class="txt">
-                        
-                        <p className="subtit">El pasaje "Lorem ipsum..." se ha extraído del texto que dice "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...", que se traduciría como 
-                            "No hay nadie que ame el dolor mismo, que lo busque y lo quiera tener, simplemente porque es el dolor..."</p>
+                <body className="homePage_cuerpo">  
+                <div className="homePage_imagenes">
+                    <div className="homePage_imagen-1">
+                    <div className="homePage_c-img">
+                        <img className="homePage_foto"src={foto1} alt=""></img>
+                        <div className="homePage_txt">
+                            
+                            <p className="homePage_subtit">GestorSub es una aplicación web que sirve para poder organizar tus suscripciones
+                            a diferentes plataformas en un mismo espacio. Además, puedes llevar un control de cuanto estas gastando en cada suscripción,
+                            así como añadir suscripciones personalizadas.</p>
 
+                        </div>
                     </div>
-                </div>
-                </div>
-                <div class="imagen-2">
-                <div class="d-img">
-                    <img class="foto2" src={foto2}  alt=""></img>
-                    <div class="txt">
-                        <p className="subtit">El pasaje "Lorem ipsum..." se ha extraído del texto que dice "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...", que se traduciría como 
-                            "No hay nadie que ame el dolor mismo, que lo busque y lo quiera tener, simplemente porque es el dolor..."</p>
+                    </div>
+                    <div className="homePage_imagen-2">
+                    <div className="homePage_d-img">
+                        <img className="homePage_foto" src={foto2}  alt=""></img>
+                        <div className="homePage_txt">
+                            <p className="homePage_subtit">Somos un grupo de estudiantes de ingeniería informática, cada uno con diferentes aptitudes,
+                            que se han propuesto realizar una página web totalmente funcional desde cero, para así poder aplicar
+                            las capacidades adquiridas a lo largo de la carrera de ingeniería informática. </p>
 
+                        </div>
                     </div>
-                </div>
-                </div>
-                </div>
-               
+                    </div>
+                    
+                </div> 
+                
                 </body>
-
-
-                <footer>
-                <div class="pie">2021 &copy; 
-                <img class="logo" src={logo} alt=""></img>
+ 	        <footer className="homepage-footer">
+                <div class="homepage-pie">2021 &copy; 
+                <img class="homepage-logo" src={logo} alt=""></img>
                 
                 </div>
 
