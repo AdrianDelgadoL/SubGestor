fe9d96b555fdcf7a177226815556d5178795b85f
1494116 <daniel.calvor@e-campus.uab.cat>
2021-05-06 22:02:47 +0200

    popUp recordar contraseña validacion y mensaje de completado hechos

--- sub-gestor-code/src/pages/login/popup.js
+++ sub-gestor-code/src/pages/login/popup.js
@@ -1,25 +1,68 @@
 import React,{useState} from 'react'
 import './popup.css'
 const Popup = (props) =>{
-    const [datos,setDatos]=useState({
-        correo: ''
-    })
 
-    const handleInputChange=(event)=>{
-        console.log(event.target.value)
-        setDatos({
-            ...datos,
-            [event.target.name]: event.target.value
+    const emailRegex = RegExp(
+        /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+      );
+    const [email,setEmail]=useState('');
+    const  [emailError,setEmailError]=useState('');
+    const [formError, setFormError] = useState('');
+    const [mensaje,setMensaje]=useState('');
+  
 
-        })
+    const formValid = () => {
+        if (emailError.length > 0 || email.length === 0){
+            return false;
+        }else{
+            return true;
+        }
+      };   
+      
+
+    const handleChange=(event)=>{
+        const {name,value}=event.target;
+        setEmail(prevEmail=>({
+            ...prevEmail,
+            [name]:value
+
+            
+        }))
+        switch (name) {
+            case "email":
+              if(emailRegex.test(value)) {
+                console.log("hola que tal");
+              } else {
+                setEmailError("Dirección de email incorrecta");
+              }
+              setEmail(value);
+              break;
+            default:
+              break;
+              
+          };
+        
+    }
+
+    const cambio=()=>{
+        props.setTrigger(false)
+        setEmail(null);
+        setEmailError(0);
+        setMensaje('');
+        setFormError('');
 
     }
-    const enviarDatos=(event)=>{
+    const handleSubmit=(event)=>{
         event.preventDefault();
-        console.log(datos);
 
+        if(formValid()){
+            setMensaje("Revisa tu correo");
+        }else{
+            setFormError("El formulario contiene errores")
+        }
+        console.log(email);
     }
-
+    
     return (props.trigger) ? (
         <div className="popup">
             <div className="popup-inner">
@@ -29,21 +72,32 @@ const Popup = (props) =>{
 
                     Una vez hecho esto, ve a mirar tu correo electrónico y sigue los pasos indicados.
                 </p>
-                <form>
-                    <label>Correo electrónico: </label>
+                <form noValidate className="popup-formulario" >
                     <input 
+                    className={emailError.length > 0 ? "error" : null}
                     type="email"
-                    required 
                     placeholder="Introduce tu email"
-                    name="correo"
-                    onChange={handleInputChange}/>
+                    name="email"
+                    required
+                    value={email}
+                    onChange={handleChange}
+                            />
+                   
                     <input 
                     type="submit" 
                     value="Enviar" 
-                     onClick={()=>props.setTrigger(false)}
-                     onSubmit={enviarDatos}/>
+                    onClick={handleSubmit}
+                    
+                     />
+                    {emailError.length > 0 && (<span className="popup-errorMessage">{emailError}</span>)}
+                    {formError.length > 0 && (
+                      <span className="popup-errorMessage">{formError}</span>
+                    )}
+                   
                 </form>
-                <button className="close-btn" onClick={()=>props.setTrigger(false)}>X</button>
+                <button className="close-btn" onClick={cambio}>X</button>
+                {mensaje.length > 0 && (
+                      <span className="popup-exitoso">{mensaje}</span>)}
                 {props.children}
                 
 
