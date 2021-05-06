1b4a8b666e92d60c7673d82a44f7c5fd54f20cc5
VictorGonzalezEscriba <victor.2418@gmail.com>
2021-05-06 13:50:22 +0200

    Pantalla seleccion plantilla responsive

--- sub-gestor-code/src/pages/selectPlantilla/selectPlantilla.component.js
+++ sub-gestor-code/src/pages/selectPlantilla/selectPlantilla.component.js
@@ -3,7 +3,15 @@ import {Link} from 'react-router-dom';
 import './selectPlantilla.css';
 import { useAuthState, useAuthDispatch } from '../../context/context';
 import axios from "axios";
-import fotoPlantilla from '../assets/crear-sub.png'
+import amazon from '../assets/amazon.jpg';
+import apple from '../assets/apple-music.jpg';
+import dazn from '../assets/dazn.jpg';
+import hbo from '../assets/hbo.png';
+import netflix from '../assets/netflix.jpg';
+import psn from '../assets/psn-plus.jpg';
+import spotify from '../assets/spotify.png';
+import wow from '../assets/wow.jpg';
+import xbox from '../assets/xbox-live.jpg';
 
 
 const selectPlantilla = (props) => {
@@ -20,6 +28,53 @@ const selectPlantilla = (props) => {
             <hr className="selectPlantilla-separator"/>
             <div class="selectPlantilla-plantillas">
                 <h1 className="selectPlantilla-subtitulo">Utiliza una plantilla creada</h1>
+                <div className="selectPlantilla-grid-container">
+                    <div class="selectPlantilla-grid-area" id="selectPlantila-grid-amazon">
+                        <a href="/home">
+                            <img src={amazon} class="selectPlantilla-plantilla-img" type="submit"></img>
+                        </a>
+                    </div>
+                    <div class="selectPlantilla-grid-area" id="selectPlantila-grid-apple">
+                        <a href="/home">
+                            <img src ={apple} class="selectPlantilla-plantilla-img"></img>
+                        </a>
+                    </div>
+                    <div class="selectPlantilla-grid-area" id="selectPlantila-grid-dazn">
+                        <a href="/home">
+                            <img src ={dazn} class="selectPlantilla-plantilla-img"></img>
+                        </a>
+                    </div>
+                    <div class="selectPlantilla-grid-area" id="selectPlantila-grid-hbo">
+                        <a href="/home">
+                            <img src ={hbo} class="selectPlantilla-plantilla-img"></img>
+                        </a>
+                    </div>
+                    <div class="selectPlantilla-grid-area" id="selectPlantila-grid-netflix">
+                        <a href="/home">
+                            <img src ={netflix} class="selectPlantilla-plantilla-img"></img>
+                        </a>
+                    </div>
+                    <div class="selectPlantilla-grid-area" id="selectPlantila-grid-psn">
+                        <a href="/home">
+                            <img src ={psn} class="selectPlantilla-plantilla-img"></img>
+                        </a>
+                    </div>
+                    <div class="selectPlantilla-grid-area" id="selectPlantila-grid-spotify">
+                        <a href="/home">
+                            <img src ={spotify} class="selectPlantilla-plantilla-img"></img>
+                        </a>
+                    </div>
+                    <div class="selectPlantilla-grid-area" id="selectPlantila-grid-wow">
+                        <a href="/home">
+                            <img src ={wow} class="selectPlantilla-plantilla-img"></img>
+                        </a>
+                    </div>
+                    <div class="selectPlantilla-grid-area" id="selectPlantila-grid-xbox">
+                        <a href="/home">
+                            <img src ={xbox} class="selectPlantilla-plantilla-img"></img>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     )
