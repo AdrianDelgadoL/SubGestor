bf985a02a2a64bb8a65deb336629875d3ffae614
Victor <victor.gonzalezes@e-campus.uab.cat>
2021-05-12 13:30:20 +0200

    Confirmo pa hacer pull

--- sub-gestor-code/src/pages/modPerfil/modPerfil.component.js
+++ sub-gestor-code/src/pages/modPerfil/modPerfil.component.js
@@ -10,35 +10,43 @@ const Perfil = (props) => {
     const dispatch = useAuthDispatch();
     const [currency, setCurrency] = useState(null);
     const [frequency, setFrequency] = useState(null);
+    const [backendError, setBackendError] = useState('');
     
     
-    /* Función para borrar el usuario */
+    // Función para borrar el usuario 
     const eliminarPerfil = () => {
         axios.delete('http://localhost:4000/user', {headers: {"x-auth-token": userDetails.token}})
-        .then(response => {
+        .then(response => { // 2xx OK 
             console.log(response.data)
             dispatch({ type: 'LOGOUT' })
             props.history.push('/');
-        })  
+        }) 
+        .catch(err => { // El response devuelve otra cosa distinta a 2xx, hay error, 401 error de token
+            if (err.response.status === 401) {
+                dispatch({type: 'AUTH_ERROR', error: err.response.data})
+                return;
+            }
+            // Sino muestra mensaje de error 
+            if (err.response) {
+                setBackendError(err.response.data.msg)
+            }
+        });
     }
 
-    /* Función para modificar el perfil */
-    const modificarPerfil = async (e) => {
-        e.preventDefault();
-        let data = new FormData();
+    // Función para modificar el perfil
+    const modificarPerfil =  () => {
+        let data = {prefered_currency: currency, frequency: frequency};
 
-        //
-        data.append('currency', currency)
-        data.append('frequency', frequency)
-
-        //
-        axios.put('https://localhost:4000/user/configuration', {data: data,headers:{"x-auth-token": userDetails.token}})
+        // Actualizar los valores
+        axios.put('https://localhost:4000/user/configuration', {data: data, headers:{"x-auth-token": userDetails.token}})
         .then(response => {
             console.log(response.data)
             dispatch({type: 'AUTH_ERROR'})
+            props.history.push('/');
         })
     }
 
+    /* Función para actualizar los valores de currency y frequency*/
     const handleChange = async (e) => {
         e.preventDefault();
         const {name, value } = e.target;
@@ -76,7 +84,7 @@ const Perfil = (props) => {
                 <hr className="modPerfil-separator"/>
                 <div className="modPerfil-bottom">
                     <div className="modPerfil-bottom-pswbutton">
-                        {/*Enlazar boton con la pagina de cambiar contraseña*/}
+                        {/*Enlazar boton con la pagina de cambiar contraseña /change-password*/}
                         <button className="modPerfil-bottom-psw" type="submit">Cambiar contraseña</button>
                     </div>
                     <div className="modPerfil-bottom-removebtn">
