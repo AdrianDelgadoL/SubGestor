a341804d09061a11478633a11aaa6a86fd97d95f
janTugores <jan.tugores@e-campus.uab.cat>
2021-05-14 10:37:41 +0200

    Opcion de mostrar las suscripciones en mes/año

--- sub-gestor-code/src/pages/subscriptions/SubList.components.js
+++ sub-gestor-code/src/pages/subscriptions/SubList.components.js
@@ -12,7 +12,9 @@ const SubList = (props) => {
     const dispatch = useAuthDispatch()
     const userToken = userDetails.token
     const [tarjetas, setTarjetas] = useState(null)
+    const [selector, setSelector] = useState(null)
     const imageRoute = "/images/"
+    const defaultSelect = useState("mes")
 
     useEffect(() => {
         axios.get('http://localhost:4000/subscription/', {headers: {"x-auth-token": userToken}})
@@ -20,9 +22,25 @@ const SubList = (props) => {
                 if(response.data.length == 0) {
                     setTarjetas(<h1>Parece que aún no existe ninguna suscripción</h1>)
                 } else {
+                    const handlePeriodChange = function (e) {
+                        const option = e.target.value
+                        setTarjetas(response.data.map(tarjeta => (                   
+                            <div className="row-md-2" key={tarjeta._id}>
+                                <Subscription title={tarjeta.name} imageSource={imageRoute + tarjeta.img_src} card_price={tarjeta.price} 
+                                payment_type={tarjeta.currency} charge_date={tarjeta.charge_date} sub_id={tarjeta._id} url={tarjeta.url} period={option} />
+                            </div>
+                        )))
+                    }
+                    setSelector(
+                        <select onChange={handlePeriodChange} className="btn dropdown-menu-right">
+                            <option value="mes" defaultValue>Mensual</option>
+                            <option value="año">Anual</option>
+                        </select>
+                    )
                     setTarjetas(response.data.map(tarjeta => (                   
                         <div className="row-md-2" key={tarjeta._id}>
-                            <Subscription title={tarjeta.name} imageSource={imageRoute + tarjeta.img_src} card_price={tarjeta.price} payment_type={tarjeta.currency} charge_date={tarjeta.charge_date} sub_id={tarjeta._id} url={tarjeta.url}/>
+                            <Subscription title={tarjeta.name} imageSource={imageRoute + tarjeta.img_src} card_price={tarjeta.price} 
+                            payment_type={tarjeta.currency} charge_date={tarjeta.charge_date} sub_id={tarjeta._id} url={tarjeta.url} period={defaultSelect} />
                         </div>
                     )))
                 }
@@ -37,6 +55,9 @@ const SubList = (props) => {
     return(
         <div>
             <h2>Tus suscripciones</h2>
+            <div className="subList-selector">
+                {selector}
+            </div>
             <div className="subList-container container">             
                 {tarjetas}                
             </div>
--- sub-gestor-code/src/pages/subscriptions/Subscription.components.js
+++ sub-gestor-code/src/pages/subscriptions/Subscription.components.js
@@ -3,7 +3,7 @@ import './Subscription.component.css'
 import {Link} from 'react-router-dom';
 import moment from 'moment'
 
-function Subscription({title,imageSource,card_price,payment_type, charge_date, sub_id, url}){
+function Subscription({title,imageSource,card_price,payment_type, charge_date, sub_id, url, period}){
     const detailLink = "/subDetail/" + sub_id
     function selectCurrency() {
         switch(payment_type) {
@@ -15,6 +15,18 @@ function Subscription({title,imageSource,card_price,payment_type, charge_date, s
             return ""
         }
     }
+
+    function selectPeriod() {
+        if(period == "año")
+            return "año"
+        return "mes"
+    }
+
+    function price() {
+        if(period == "año")
+            return card_price*12
+        return card_price
+    }
     
     return(
     <div class="Subscription-card-container">
@@ -23,7 +35,7 @@ function Subscription({title,imageSource,card_price,payment_type, charge_date, s
         </div>
         <div class="Subscription-text-col text-col">
             <h3 className="Subscription-titulo">{title}</h3>
-            <p className="Subscription-precio" >{card_price}{selectCurrency()}</p>
+            <p className="Subscription-precio" >{price()}{selectCurrency()}/{selectPeriod()}</p>
             <p className="Subscription-charge_date" >Próximo cobro: {moment(charge_date).format("DD/MM/YYYY")}</p>
             <Link to ={detailLink} className="btn btn-outline-secondary border-0" >Consultar suscripción</Link>
             {url.length == 0 && (
